c0e4bea17f05761a0d2efaf37e7d8141ec217834
J0hnZMT <tuddaojohnzel@gmail.com>
2019-10-28 17:04:14 +0800

    updated

--- search/search.py
+++ search/search.py
@@ -5,6 +5,19 @@ import csv
 import getpass
 
 
+class Password:
+
+    DEFAULT = 'Prompt if not specified'
+
+    def __init__(self, passwd):
+        if passwd == self.DEFAULT:
+            passwd = getpass.getpass('Github Password: ')
+        self.passwd = passwd
+
+    def __str__(self):
+        return self.passwd
+
+
 def csv_date_time():
     # add date and time to the output
     now = datetime.now()
@@ -13,10 +26,8 @@ def csv_date_time():
     return csv_filename_now
 
 
-def auth(user, passwd, term):
+def auth(term, user, passwd):
     requests.get('https://api.github.com/user', auth=(user, passwd))
-    r = requests.get('https://api.github.com/rate_limit')
-    print(r.content)
     search(term)
 
 
@@ -27,27 +38,29 @@ def search(term):
         field_header = ['Project Name', 'Description', 'Url', 'Programming Language', 'Date Updated']
         csv_writer = csv.DictWriter(csv_file, fieldnames=field_header)
         csv_writer.writeheader()
-        for page in range(1, 15):
-            term_to_search = {'q': term, 'page': page}
-            search_term = requests.get('https://api.github.com/search/repositories', params=term_to_search)
-            print(search_term.url)
-            json_result = search_term.json()
-            items = json_result['items']
-            for item in items:
-                result = {'Project Name': item['name'], 'Description': item['description'], 'Url': item['html_url'],
-                          'Programming Language': item['language'], 'Date Updated': item['updated_at']}
-                csv_writer.writerow(result)
-    print("Search Successful!")
+        try:
+            for page in range(1, 12):
+                term_to_search = {'q': term, 'per_page': 100, 'page': page}
+                search_term = requests.get('https://api.github.com/search/repositories', params=term_to_search)
+                json_result = search_term.json()
+                items = json_result['items']
+                for item in items:
+                    result = {'Project Name': item['name'], 'Description': item['description'], 'Url': item['html_url'],
+                              'Programming Language': item['language'], 'Date Updated': item['updated_at']}
+                    csv_writer.writerow(result)
+        except KeyError as e:
+            print("Search Done! Search results save in {}".format(filename))
 
 
 def main():
     """ Main Function of the program """
     parser = argparse.ArgumentParser()
-    parser.add_argument("user", nargs='?', help="Your username in Github", type=str)
-    parser.add_argument("passwd", nargs='?', help="Your Password in Github", type=str)
     parser.add_argument("term", nargs='?', help="The term you want to search in Github", type=str)
+    parser.add_argument('-u', '--username', nargs='?', help="Your username in Github", type=str)
+    parser.add_argument('-p', '--password', type=Password, help='Specify password',
+                        default=Password.DEFAULT)
     args = parser.parse_args()
-    auth(args.user, args.passwd, args.term)
+    auth(args.term, args.username, args.password)
 
 
 if __name__ == '__main__':
